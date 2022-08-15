31b20abe8dd26ca6e13da0a53ff560bc5a0744c4
Nikolay Neupokoev <neupokoev.n@gmail.com>
2022-08-14 23:15:14 -0700

    Make a page :o

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GettingStarted msg="Welcome to GutHub Brush!"/>
+  <footer>
+    <h3>Guthub Brush</h3>
+    <ul>
+      <li>This is a fun little Rust 🦀 learning project</li>
+      <li>developed by <a href="https://twitter.com/mikolasan">@mikolasan</a></li>
+    </ul>
+  </footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GettingStarted from './components/GettingStarted.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GettingStarted
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 4rem;
+}
+img {
+  max-height: 12rem;
+}
+footer {
+  margin-top: 8rem;
+  padding: 3rem;
+  background-color: #2c3e50;
+  color: #e3edf8;
+}
+a {
+  color: #e3edf8;
+}
+ul {
+  list-style-type: none;
 }
 </style>
---
+++ src/components/GettingStarted.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      
+    </p>
+    <h3>Choose a word</h3>
+    <ul>
+      <li>not more than 10 symbols to fit it on one screen</li>
+      <li>this word will represent your profile, so be caution in your choise</li>
+    </ul>
+    <h3>Find a starting week</h3>
+    <ul>
+      <li>to tell the algorithm where to start painting points</li>
+      <li>can be chosen just a number of the week in a month</li>
+      <li>make sure that the text will not overlay with other activity in the account</li>
+    </ul>
+    <h3>Give access to the account</h3>
+    <ul>
+      <li>default repository name is <strong>guthub-brush-activity</strong> can be changed of course</li>
+      <li>the repository is automatically created</li>
+      <li>the repository can be automatically deleted to recreate activity with another starting date or another word</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GettingStarted',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 5rem;
+}
+h3 {
+  font-size: 2rem;
+  margin: 3rem 0 0;
+}
+ul {
+  padding: 0;
+}
+li {
+  margin: 1rem;
+}
+a {
+  color: #42b983;
+}
+</style>
